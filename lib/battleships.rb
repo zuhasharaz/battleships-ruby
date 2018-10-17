d39e16a42a9c68a8f7496481adb6ed89999f85ef
ramseymahgoub <ramseymahgoub@gmail.com>
2018-10-17 12:02:06 +0100

    adds player and computer grids and ships and players ability to fire.

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,3 +1,58 @@
-def battle
-  "hello"
+class Battleship
+
+  def initialize
+    @their_ships = [["b1"]]
+    @their_grid = []
+    @my_grid = []
+    @my_ships = [["a1"]]
+  end
+
+  def my_ships
+    @my_ships
+  end
+
+  def my_grid
+    @my_grid
+  end
+
+  def their_grid
+    @their_grid
+  end
+
+  def fire(coord)
+    hit = @their_ships.any? do |ship|
+      ship.include?(coord)
+    end
+    @their_grid << Cell.new(coord,hit)
+    hit
+  end
+
+  def their_fire(coord)
+    hit = @my_ships.any? do |ship|
+      ship.include?(coord)
+    end
+    @my_grid << Cell.new(coord,hit)
+    hit
+  end
+
+  def next_turn
+
+  end
+
 end
+
+class Cell
+  def initialize(coord, hit)
+    @coord = coord
+    @hit = hit
+  end
+
+  def hit
+    @hit
+  end
+
+  def coord
+    @coord
+  end
+end
+
--- spec/battleships_spec.rb
+++ spec/battleships_spec.rb
@@ -1,6 +1,46 @@
 require 'battleships'
 RSpec.describe 'Battleships' do
-  it 'should do something but i am not sure what' do
-    expect(battle).to eq("hello")
+  it 'should return an array of one ship.' do
+    battleship = Battleship.new
+    expect(battleship.my_ships).to eq([["a1"]])
+  end
+
+  it 'should return an array of hits on my_grid.' do
+    battleship = Battleship.new
+    expect(battleship.my_grid).to eq([])
+  end
+
+  it 'should return an array of hits on their_grid.' do
+    battleship = Battleship.new
+    expect(battleship.their_grid).to eq([])
+  end
+  
+  it 'should fire at a grid cell and return miss.' do
+    battleship = Battleship.new
+    expect(battleship.fire("c1")).to eq(false)
+  end
+
+  it 'should fire at a grid cell and return miss.' do
+    battleship = Battleship.new
+    expect(battleship.fire("c2")).to eq(false)
+  end
+
+  it 'should fire at a grid cell and return hit.' do
+    battleship = Battleship.new
+    expect(battleship.fire("b1")).to eq(true)
+  end
+
+  it 'should return their grid their_grid with shot history.' do
+    battleship = Battleship.new
+    battleship.fire("b1")
+    expect(battleship.their_grid[0].hit).to eq(true)
+    expect(battleship.their_grid[0].coord).to eq("b1")
+  end
+
+  it 'battleships next_turn should make the computer fire and miss.' do
+    battleship = Battleship.new
+    battleship.next_turn
+    expect(battleship.my_grid[0].hit).to eq(false)
+    expect(battleship.my_grid[0].coord).to eq("a2")
   end
 end
